3e2c09fef14a52c9f9540057373a05ef11502b50
Линар <159605589+d1ssmuss@users.noreply.github.com>
2025-02-11 12:28:28 +0400

    Update 6 kyu Is Integer Array.py

--- Python/6 kyu Is Integer Array.py
+++ Python/6 kyu Is Integer Array.py
@@ -23,3 +23,34 @@ print(is_int_array([-11, -12, -13, -14]))
 print(is_int_array([1.0, 2.0, 3.0]))
 print(is_int_array([1, 2, None]))
 print("1", is_int_array(True))
+
+
+#
+"""
+Моё 1-ое решение
+def is_int_array(arr):
+    """
+    Напишите функцию, которая:
+        возвращает значение true, если каждый элемент в массиве является целым числом или числом с плавающей точкой без десятичных знаков.
+        возвращает значение true, если массив пуст.
+        возвращает значение false для всех остальных входных данных.
+    :param arr:
+    :return:
+    """
+    if type(arr) == list:
+        if len(arr) == 0:
+            return True
+        else:
+            status = True
+            try:
+                for i in arr:
+                    if (type(i) == int) or (i % 1 == 0 and type(i) == float):
+                        continue
+                    else:
+                        return False
+            except:
+                return False
+            return status
+    else:
+        return False
+"""
