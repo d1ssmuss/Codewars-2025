7627ae00fc0eb5769ec9e6014897de71bb61cb7e
Линар <159605589+d1ssmuss@users.noreply.github.com>
2025-06-20 21:31:45 +0400

    Update 7 kyu Testing 1-2-3.py

--- Python/7 kyu Testing 1-2-3.py
+++ Python/7 kyu Testing 1-2-3.py
@@ -1,3 +1,17 @@
+"""
+Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+
+Write a function which takes a list of strings and returns each line prepended by the correct number.
+
+The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+
+Ваша команда пишет новый текстовый редактор, и вы заинтересовались реализацией нумерации строк.
+
+Напишите функцию, которая принимает список строк и возвращает перед каждой строкой правильный номер.
+
+Нумерация начинается с 1. Формат - n: строка. Обратите внимание на двоеточие и пробел между ними.
+"""
+
 def number(lines):
     return [f"{i+1}: {lines[i]}" for i in range(len(lines))]
 
