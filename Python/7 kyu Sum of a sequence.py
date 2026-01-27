db1fedfc70f5556b264267fdfa63d1cd3b38c053
Линар Аскаров <linar.askarov@list.ru>
2026-01-27 11:42:53 +0400

    Задача решена

---
+++ Python/7 kyu Sum of a sequence.py
@@ -0,0 +1,14 @@
+def sequence_sum(begin_number, end_number, step):
+    if begin_number > end_number:
+        return 0
+    else:
+        s = 0
+        for i in range(begin_number, end_number+1, step):
+            print(i)
+            s += i
+        return s
+
+
+print(sequence_sum(2,6,2))
+print(sequence_sum(13,15,2))
+print(sequence_sum(-1,20,2))
