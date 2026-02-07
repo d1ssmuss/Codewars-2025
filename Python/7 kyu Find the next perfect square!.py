7972cf55c85e31f365c1adeccd9c900ae0443513
Линар <159605589+d1ssmuss@users.noreply.github.com>
2026-02-07 23:51:19 +0400

    Add files via upload

---
+++ Python/7 kyu Find the next perfect square!.py
@@ -0,0 +1,6 @@
+def find_next_square(sq):
+    # Return the next square if sq is a square, -1 otherwise
+    return int(((sq ** 0.5) + 1) ** 2) if (sq ** 0.5).is_integer() else -1
+
+
+print(find_next_square(319225))
