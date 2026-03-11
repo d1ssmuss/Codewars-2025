45ea8b40f21ac0e0f4a504479912246db1a20671
Линар <159605589+d1ssmuss@users.noreply.github.com>
2026-03-11 20:04:41 +0400

    Add files via upload

---
+++ Python/6 kyu Convert string to camel case.py
@@ -0,0 +1,24 @@
+import re
+
+def to_camel_case(text):
+    words = []
+    word = ''
+    flag = None
+    for letter in text:
+        if letter.isalpha():
+            if flag:
+                word += letter.upper()
+                flag = False
+            else:
+                word += letter
+            words.append(word)
+            word = ''
+        else:
+            flag = True
+    return ''.join(words)
+
+
+print(to_camel_case("the-stealth-warrior"))
+print(to_camel_case("The_Stealth_Warrior"))
+print(to_camel_case("The_Stealth-Warrior"))
+print(to_camel_case("The-Stealth_Warrior"))
